ce5af49a773328d86ac6f37b0960fa0843b5a0d1
vinod sjml <vinsjml@gmail.com>
2023-11-29 08:59:06 +0530

    auto push

--- condition.sh
+++ condition.sh
@@ -1,11 +1,11 @@
 Action=$1
 case $Action in
             start)
-                echo starting the payment;;
+                echo -e "\e[32m starting the payment \e[0m";;
             stop)
-                echo stopping the payment;;
+                echo -e "\e[31m stopping the payment \e[0m";;
             restart)
-                echo restarting the payment;;
+                echo -e "\e[33m restarting the payment \e[0m";;
             *)
                 echo -e "\e[33m valid options are start, stop, restart \e[0m"
                 echo -e "\e[33m please run the script again \e[0m" ;;
