aa5fa8bb851b613f4017e45bdffa9c179b2c3529
vinod sjml <vinsjml@gmail.com>
2023-11-30 18:07:58 +0530

    auto push

--- Roboshop/components/frontend.sh
+++ Roboshop/components/frontend.sh
@@ -3,7 +3,7 @@
 set -e
 user_id=$(id -u)
 if [ $user_id -ne 0 ]; then
-     echo -e "this script installs only when it is run by a rot user or with a sudo access \n\t Example: sudo bash wrapper.sh frontend"
+     echo -e "this script installs only when it is run by a root user or with a sudo access \n\t Example: sudo bash wrapper.sh frontend"
 fi
 
 echo -e "\e[34m configuring frontend... \e[0m"
@@ -13,7 +13,7 @@ stat(){
        echo -e "\e[32m success \e[0m"
     else 
        echo -e "\e[32m failure \e[0m"   
-}
+       }
 
 yum install nginx -y &>> /tmp/frontend.logs
 echo -n "nginx installation status:" 
