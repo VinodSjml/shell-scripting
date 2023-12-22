5d4f45ba160acc9d44c9a3506bd8caa06dffc3b9
vinod sjml <vinsjml@gmail.com>
2023-12-22 07:52:48 +0530

    auto push

--- Roboshop/components/mysql.sh
+++ Roboshop/components/mysql.sh
@@ -1,3 +1,19 @@
 #!/bin/bash
 
-echo this is mysql
+source components/common.sh
+
+comp_name=mysql
+
+echo -e "\e[32m configuring ${comp_name} .... \e[0m"
+echo -n "download ${comp_name} repo: "
+curl -s -L -o /etc/yum.repos.d/mysql.repo https://raw.githubusercontent.com/stans-robot-project/mysql/main/mysql.repo
+stat $?
+
+echo -n "installing ${comp_name}:"
+yum install mysql-community-server -y
+stat $?
+
+echo -n "starting ${comp_name} service:"
+systemctl enable mysqld
+systemctl start mysqld
+stat $?
