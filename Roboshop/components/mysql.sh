11b3b76cc6fc9fdb506a2d79532b6599cbb0396f
vinod sjml <vinsjml@gmail.com>
2023-12-22 11:10:55 +0530

    auto push

--- Roboshop/components/mysql.sh
+++ Roboshop/components/mysql.sh
@@ -22,7 +22,7 @@ echo -n "exctracting default password for mysql: "
 default_password=$(grep "password" /var/log/mysqld.log | awk -F " " '{print $NF}')
 stat $?
 
-echo "show databases;" |mysql -uroot -pRoboshop@1
+echo "show databases;" |mysql -uroot -pRoboShop@1
 if [ $? -ne 0 ]; then
    echo -n "changing the default password: "
    echo "ALTER USER 'root'@'localhost' IDENTIFIED BY 'RoboShop@1'" | mysql --connect-expired-password -uroot -p$default_password &>> ${logfile}
