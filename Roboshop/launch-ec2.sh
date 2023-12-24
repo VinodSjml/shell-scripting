88169a969b49d44b5ee19116ad0a6abc0cd2e09f
vinod sjml <vinsjml@gmail.com>
2023-12-24 19:04:16 +0530

    auto push

--- Roboshop/launch-ec2.sh
+++ Roboshop/launch-ec2.sh
@@ -6,9 +6,9 @@ if [ -z $1 ]; then
     exit 1
 fi
 
-ami_id = "ami-0f75a13ad2e340a58"
-instance_type = "t2.micro"
-secgrp_id = "sg-008477cb2de2ff3b0"
+ami_id="ami-0f75a13ad2e340a58"
+instance_type="t2.micro"
+secgrp_id="sg-008477cb2de2ff3b0"
 
 echo -e "\e[32m launching ${component} server \e[0m"
 aws ec2 run-instances --image-id ${ami_id} --count 1 --instance-type ${instance_type} --security-group-ids sg-903004f8 --tags Key=Name,Value=${component}
