57ced1f1a9de8a8a7065916cbe08b8bfcc22afcc
vinod sjml <vinsjml@gmail.com>
2023-12-25 13:35:03 +0530

    auto push

--- Roboshop/launch-ec2.sh
+++ Roboshop/launch-ec2.sh
@@ -8,8 +8,11 @@ fi
 
 ami_id=$(aws ec2 describe-images --filters "Name=name,Values=DevOps-LabImage-CentOS7" | jq ".Images[].ImageId" | sed -e 's/"//g')
 instance_type="t2.micro"
-secgrp_id="sg-008477cb2de2ff3b0"
+secgrp_id=$(aws ec2 describe-security-groups --filters "Name=group-name,Values=clouddevops-allow all" | jq ".SecurityGroups[].GroupId" | sed -e 's/"//g')
+
 
 echo -e "\e[32m launching ${component} server \e[0m"
 private_ip=$(aws ec2 run-instances --image-id ${ami_id} --count 1 --instance-type ${instance_type} --security-group-ids ${secgrp_id} --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=${component}}]" | jq '.Instances[].PrivateIpAddress' | sed -e 's/"//g')
 echo "${component} server has been launched and its private ip is ${private_ip}"
+clouddevops-allow all
+aws ec2 describe-security-groups --filters "Name=group-name,Values=clouddevops-allow all"
