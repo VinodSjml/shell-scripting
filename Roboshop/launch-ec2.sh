90fe635a9d0eb6c9b263af65805f6542d5674a0c
vinod sjml <vinsjml@gmail.com>
2023-12-25 20:08:28 +0530

    auto push

--- Roboshop/launch-ec2.sh
+++ Roboshop/launch-ec2.sh
@@ -15,6 +15,6 @@ echo -e "\e[32m launching ${component} server \e[0m"
 private_ip=$(aws ec2 run-instances --image-id ${ami_id} --count 1 --instance-type ${instance_type} --security-group-ids ${secgrp_id} --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=${component}}]" | jq '.Instances[].PrivateIpAddress' | sed -e 's/"//g')
 echo "${component} server has been launched and its private ip is ${private_ip}"
 
-sed -e 's/component/${component}/' -e 's/IPAddress/${private_ip}/' route53.json > /tmp/r53.json
+sed -e "s/component/${component}/" -e "s/IPAddress/${private_ip}/" route53.json > /tmp/r53.json
 aws route53 change-resource-record-sets --hosted-zone-id ${hostedzone_id} --change-batch file:///tmp/r53.json
 echo "${component}.roboshop.internal is now ready to access"
